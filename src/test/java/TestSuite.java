5cb2a1545c21f0b6fdf56c31a2264f8ddc486f5a
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2021-01-02 16:39:22 -0300

    Import project

---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestSuite {
+    @Test
+    public void testAdd() {
+        assertEquals(45, Integer.sum(10,32));
+    }
+}
