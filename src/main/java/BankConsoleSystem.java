8d1a9b79bfc80062a4192d2bc09ecd7e912e3b6a
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2021-01-02 17:23:41 -0300

    Add initial set of failing tests

---
+++ src/main/java/BankConsoleSystem.java
@@ -0,0 +1,15 @@
+public class BankConsoleSystem {
+    private Client currentUser;
+
+    public BankConsoleSystem() {
+        this.currentUser = new Client();
+    }
+
+    public void reloadDefaults() {
+        // This resets accounts
+    }
+
+    public void interactive() {
+        // This starts the interactive flow for users
+    }
+}
--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -2,8 +2,50 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestSuite {
+    private final BankConsoleSystem system;
+
+    public TestSuite() {
+        this.system = new BankConsoleSystem();
+        this.system.reloadDefaults();
+    }
+
     @Test
-    public void testAdd() {
-        assertEquals(45, Integer.sum(10,32));
+    public void testDoDepositCLP() {
+        assertEquals(true, false);
+    }
+
+    @Test
+    public void testDoDepositUSD() {
+        assertEquals(true, false);
+    }
+
+    @Test
+    public void testDoWithdrawCLP() {
+        assertEquals(true, false);
+    }
+
+    @Test
+    public void testDoWithdrawUSD() {
+        assertEquals(true, false);
+    }
+
+    @Test
+    public void testMaxNumberOfOperations() {
+        assertEquals(true, false);
+    }
+
+    @Test
+    public void testGetHistoryOfTransactions() {
+        assertEquals(true, false);
+    }
+
+    @Test
+    public void testLogIn() {
+        assertEquals(true, false);
+    }
+
+    @Test
+    public void testLogOut() {
+        assertEquals(true, false);
     }
 }
