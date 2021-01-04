2acdd3a844b92678f6f43933a6938c9377f4132e
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2021-01-04 19:23:56 -0300

    Terminal interface done

--- src/main/java/BankConsoleSystem.java
+++ src/main/java/BankConsoleSystem.java
@@ -1,21 +1,26 @@
+import org.beryx.textio.TextIO;
+import org.beryx.textio.TextIoFactory;
+import org.beryx.textio.TextTerminal;
+
 import java.util.HashMap;
 import java.util.List;
 
 public class BankConsoleSystem {
     private Client currentUser;
     private HashMap<Integer, Client> systemUsers;
-    private final float MIN_DEPOSIT_USD = 10;
-    private final float MIN_DEPOSIT_CLP = 2000;
-    private final float MIN_WITHDRAW_USD = 10;
-    private final float MIN_WITHDRAW_CLP = 2000;
-    private final float MAX_WITHDRAW_USD = 100;
-    private final float MAX_WITHDRAW_CLP = 200_000;
+    private final ScreenManager manager;
+    protected final long MIN_DEPOSIT_USD = 10;
+    protected final long MIN_DEPOSIT_CLP = 2000;
+    protected final long MIN_WITHDRAW_USD = 10;
+    protected final long MIN_WITHDRAW_CLP = 2000;
+    protected final long MAX_WITHDRAW_USD = 100;
+    protected final long MAX_WITHDRAW_CLP = 200_000;
     private final int MAX_OPERATIONS_SESSION = 4;
     private int currentOperations;
 
     public BankConsoleSystem() {
+        manager = new ScreenManager(this);
         setup();
-        //FIXME: logging (log4j or logback)
     }
 
     private void setup() {
@@ -31,7 +36,7 @@ public class BankConsoleSystem {
 
     public SystemError deposit(long amount, boolean isUSD) {
         if (currentUser != null) {
-            if (currentOperations > MAX_OPERATIONS_SESSION) {
+            if (currentOperations >= MAX_OPERATIONS_SESSION) {
                 return SystemError.SESSION_NUMBER_OF_OPERATIONS_EXCEEDED;
             }
             Operation op;
@@ -65,7 +70,7 @@ public class BankConsoleSystem {
 
     public SystemError withdraw(long amount, boolean isUSD) {
         if (currentUser != null) {
-            if (currentOperations > MAX_OPERATIONS_SESSION) {
+            if (currentOperations >= MAX_OPERATIONS_SESSION) {
                 return SystemError.SESSION_NUMBER_OF_OPERATIONS_EXCEEDED;
             }
             Operation op;
@@ -145,6 +150,141 @@ public class BankConsoleSystem {
     }
 
     public void interactive() {
-        // This starts the interactive flow for users
+        manager.login();
+    }
+
+    private static class ScreenManager {
+        private final BankConsoleSystem system;
+        private final TextIO textIO;
+        private final TextTerminal<?> terminal;
+
+        public ScreenManager(BankConsoleSystem system) {
+            this.system = system;
+            textIO = TextIoFactory.getTextIO();
+            terminal = textIO.getTextTerminal();
+        }
+
+        public void login() {
+            terminal.println("===============================[CREDENCIALES]==================================");
+            terminal.println("Bienvenido a Banco Azul, debe iniciar sesion para comenzar:\n");
+            int uid = textIO.newIntInputReader().withMinVal(1).read("ID del usuario =>");
+            String password = textIO.newStringInputReader().withInputMasking(true).read("Contrasena =>");
+            SystemError rc = system.logIn(uid, password);
+            if (rc == SystemError.OK) {
+                terminal.println("Bienvenido!");
+                screen0();
+            } else {
+                terminal.println("ERROR: " + rc);
+                login();
+            }
+        }
+
+        private void screen0() {
+            terminal.println("==================================[CUENTAS]====================================");
+            terminal.println("Tipo de Cuenta\t\tMonto Disponible\n");
+            terminal.println("Cuenta CLP\t\t$" + system.getCurrentUser().getBalance(false));
+            terminal.println("Cuenta USD\t\t$" + system.getCurrentUser().getBalance(true));
+            terminal.println("=====================================[MENU]====================================");
+            terminal.println("Bienvenido a Banco Azul, selecciona operacion a realizar:\n");
+            terminal.println("1 - Deposito");
+            terminal.println("2 - Retiro");
+            terminal.println("3 - Ver transacciones");
+            terminal.println("4 - Cerrar sesion");
+            int option = textIO.newIntInputReader().withMinVal(1).withMaxVal(4).read("Opcion =>");
+            switch (option) {
+                case 1:
+                    screen1();
+                    break;
+                case 2:
+                    screen2();
+                    break;
+                case 3:
+                    screen3();
+                    break;
+                case 4:
+                    screen4();
+                    break;
+            }
+        }
+
+        private void screen1() {
+            terminal.println("===================================[DEPOSITO]==================================");
+            terminal.println("Selecciona el tipo de cuenta sobre la cual operar:\n");
+            terminal.println("1 - Cuenta CLP");
+            terminal.println("2 - Cuenta USD");
+            int account_option = textIO.newIntInputReader().withMinVal(1).withMaxVal(2).read("Opcion =>");
+            terminal.println("Ingresa el monto a depositar:\n");
+            long value;
+            SystemError rc;
+            if (account_option == 1) {
+                value = textIO.newLongInputReader().withMinVal(system.MIN_DEPOSIT_CLP).withMaxVal(Long.MAX_VALUE).read("Monto =>");
+                rc = system.deposit(value, false);
+            } else {
+                value = textIO.newLongInputReader().withMinVal(system.MIN_DEPOSIT_USD).withMaxVal(Long.MAX_VALUE).read("Monto =>");
+                rc = system.deposit(value, true);
+            }
+            if (rc == SystemError.OK) {
+                terminal.println("Deposito realizado correctamente");
+            } else {
+                terminal.println("ERROR: " + rc);
+            }
+            screen0();
+        }
+
+        private void screen2() {
+            terminal.println("====================================[RETIRO]===================================");
+            terminal.println("Selecciona el tipo de cuenta sobre la cual operar:\n");
+            terminal.println("1 - Cuenta CLP");
+            terminal.println("2 - Cuenta USD");
+            int account_option = textIO.newIntInputReader().withMinVal(1).withMaxVal(2).read("Opcion =>");
+            terminal.println("Ingresa el monto a retirar:\n");
+            long value;
+            SystemError rc;
+            if (account_option == 1) {
+                value = textIO.newLongInputReader().withMinVal(system.MIN_WITHDRAW_CLP).withMaxVal(system.MAX_WITHDRAW_CLP).read("Monto =>");
+                rc = system.withdraw(value, false);
+            } else {
+                value = textIO.newLongInputReader().withMinVal(system.MIN_WITHDRAW_USD).withMaxVal(system.MAX_WITHDRAW_USD).read("Monto =>");
+                rc = system.withdraw(value, true);
+            }
+            if (rc == SystemError.OK) {
+                terminal.println("Deposito realizado correctamente");
+            } else {
+                terminal.println("ERROR: " + rc);
+            }
+            screen0();
+        }
+
+        private void screen3() {
+            terminal.println("================================[HISTORIAL]====================================");
+            terminal.println("Mostrando el historial de transacciones realizadas:\n");
+            terminal.println("Fecha\t\t\tTipo de Cuenta\t\tTipo Operacion\t\tMonto Operacion\n");
+            for (Operation op: system.getCurrentUser().getHistory()) {
+                if (op.isUSD()) {
+                    if (op.isDeposit()) {
+                        terminal.println(op.getTimestamp() + "\tUSD\t\t\tDeposito\t\t" + op.getValue());
+                    } else {
+                        terminal.println(op.getTimestamp() + "\tUSD\t\t\tRetiro\t\t\t" + op.getValue());
+                    }
+                } else {
+                    if (op.isDeposit()) {
+                        terminal.println(op.getTimestamp() + "\tCLP\t\t\tDeposito\t\t" + op.getValue());
+                    } else {
+                        terminal.println(op.getTimestamp() + "\tCLP\t\t\tRetiro\t\t\t" + op.getValue());
+                    }
+                }
+            }
+            screen0();
+        }
+
+        private void screen4() {
+            SystemError rc = system.logOut();
+            if (rc == SystemError.OK) {
+                terminal.println("Ha cerrado sesion correctamente. Gracias por preferirnos!");
+            } else {
+                terminal.println("ERROR: " + rc);
+            }
+            login();
+        }
     }
 }
--- src/main/java/Operation.java
+++ src/main/java/Operation.java
@@ -1,12 +1,21 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Operation {
     private final long value;
     private final boolean isUSD;
     private final boolean isDeposit;
+    private final Date timestamp;
+    private final DateFormat df;
 
     public Operation(boolean isDeposit, boolean isUSD, long value) {
         this.value = value;
         this.isDeposit = isDeposit;
         this.isUSD = isUSD;
+        timestamp = Calendar.getInstance().getTime();
+        df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
     }
 
     public long getValue() {
@@ -19,5 +28,9 @@ public class Operation {
 
     public boolean isUSD() {
         return isUSD;
+    }
+
+    public String getTimestamp() {
+        return df.format(timestamp);
     }
 }
